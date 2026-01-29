5569411f4c73f0a83d1b5fb53de3a0ef1badbc8e
kimminjung <ekfkawnl644@gmail.com>
2026-01-29 22:22:49 +0900

    feat:날짜 선택

---
+++ javascript/SelectDate.js
@@ -0,0 +1,27 @@
+
+const games = [
+  { date: "01.10", time: "18:30", home: "LG", away: "삼성" },
+  { date: "01.11", time: "18:30", home: "LG", away: "두산" },
+  { date: "01.12", time: "18:30", home: "LG", away: "기아" },
+  { date: "01.13", time: "18:30", home: "LG", away: "넥센" }
+];
+
+const list = document.getElementById('game-list');
+
+games.forEach(game=>{
+    const item = document.createElement("div");
+    const info = document.createElement("div");
+    const btn = document.createElement("button");
+
+   info.textContent = `${game.date} ${game.time} ${game.home} VS ${game.away}`;
+
+    btn.textContent = "예매";
+
+    item.appendChild(info);
+    item.appendChild(btn);
+    list.appendChild(item);
+});
+
+
+
+
