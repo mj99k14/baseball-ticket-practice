6b644b49e50a7559a6898448329464e8e6e702fc
kimminjung <ekfkawnl644@gmail.com>
2026-02-13 18:04:50 +0900

    feat: 가격 확인화면 기능 구현

--- javascript/SelectDate.js
+++ javascript/SelectDate.js
@@ -1,8 +1,8 @@
 const games = [
-  { date: "01.10", time: "18:30", home: "LG", away: "삼성" },
-  { date: "01.11", time: "18:30", home: "LG", away: "두산" },
-  { date: "01.12", time: "18:30", home: "LG", away: "기아" },
-  { date: "01.13", time: "18:30", home: "LG", away: "넥센" },
+  { date: "01월10", time: "18:30", home: "LG", away: "삼성" },
+  { date: "01월11", time: "18:30", home: "LG", away: "두산" },
+  { date: "01월12", time: "18:30", home: "LG", away: "기아" },
+  { date: "01월13", time: "18:30", home: "LG", away: "넥센" },
 ];
 
 const list = document.getElementById("game-list");
@@ -20,14 +20,22 @@ games.forEach((game) => {
         <button class="res-button">예매</button>
     `;
 
-  // 어르신들은 버튼뿐만 아니라 '카드 전체'를 눌러도 이동되는 것이 훨씬 편합니다.
+  // 카드전체 눌러도 넘어갈수잇음
   item.addEventListener("click", () => {
     appState.date = game.date;
+    //session에 저장
+    sessionStorage.setItem("GameDate",appState.date);
     appState.time = game.time;
+    sessionStorage.setItem("GameTime",appState.time);
     appState.home = game.home;
+    sessionStorage.setItem("GanmeHome",appState.home);
     appState.away = game.away;
+    sessionStorage.setItem("GanmeAway",appState.away);
+
+    sessionStorage.setItem("GameArea",appState.area);
     location.href = "Select-Seat.html";
   });
-
+  
   list.appendChild(item);
+
 });
--- javascript/price.js
+++ javascript/price.js
@@ -2,6 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
   // 좌석 개수 표시 부분
   // 글자를 보여줄 곳
   const dispalyCountSeat = document.getElementById("seat-count");
+  const displySeatPrice1 = document.getElementById("seat-price1");
+  const displyGameData =document.getElementById("Gamedate");
+  const displySeatPrice2 = document.getElementById("seat-price2");
+
+  const priceValue =(sessionStorage.getItem("Seatprice"));
+  const gameDate = (sessionStorage.getItem("GameDate"));
 
   //  만약 요소를 못 찾았다면 콘솔에 에러를 띄웁니다.
   if (!dispalyCountSeat) {
@@ -10,13 +16,18 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   // localStorage에서 좌석 개수 가져오기
-  const countValue = parseInt(
-    sessionStorage.getItem("selectedSeatCount") || "0",
-    10,
-  ); //닫으면 사라지게 할려고
+  const countValue =
+  Number(sessionStorage.getItem("selectedSeatCount")) || 0;
 
   // 글씨 집어넣기
   dispalyCountSeat.textContent = `총${countValue}석 입니다 `;
+  displySeatPrice1.textContent =`티켓 총액: ${priceValue}원`;
+  displyGameData.textContent =`일시: ${gameDate}`
+  displySeatPrice2.textContent =`총 결제금액 ${priceValue}원`;
 
   console.log("표시할 좌석 개수:", countValue);
+  console.log(sessionStorage.getItem("Seatprice"));
+
+
+  
 });
